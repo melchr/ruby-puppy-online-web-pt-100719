bf4d36c3c2e484b9c7a4f0106f9ad82e45eee8f7
Melissa Christman <melchrstmn@gmail.com>
2019-10-27 18:22:37 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.print_all
+    @@all.each { |doggy| puts doggy.name}
+  end
+
+  def save
+    @@all << self
+  end
+
+end
